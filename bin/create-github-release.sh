83026c1a2f64f7224a46a4f50987215480a6a22c
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-02-11 11:55:29 +0000

    feat: added create github release script

--- bin/create-github-release.sh
+++ bin/create-github-release.sh
@@ -1,15 +1,50 @@
 #!/usr/bin/env bash
 
+getNextReleaseVersion() {
+  repoName=$1
+  currentVersion=$(curl --silent "https://api.github.com/repos/hmcts/${repoName}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+  IFS='.' read -a versionParts <<< "$currentVersion"
+  patchVersion=$((versionParts[2] + 1))
+  echo "${versionParts[0]}.${versionParts[1]}.${patchVersion}"
+}
+
+getToken() {
+  az keyvault secret show --vault-name infra-vault-nonprod --name hmcts-github-apikey --query value -o tsv
+}
+
+createNewRelease() {
+  repoName=$1
+  token=$(getToken)
+  nextReleaseVersion=$(getNextReleaseVersion $1)
+
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${token}" \
+    https://api.github.com/repos/hmcts/${repoName}/releases \
+    -d "{\"tag_name\":\"${nextReleaseVersion}\",\"name\":\"${repoName}-v${nextReleaseVersion}\",\"body\":\"${BUILD_URL}\"}" \
+    | docker run --rm --interactive stedolan/jq '.id'
+}
+
+uploadReleaseAsset() {
+  repoName=$1
+  releaseId=$2
+  assetName=$3
+  token=$(getToken)
+
+  curl \
+    -X POST \
+    -H "Authorization: token ${token}" \
+    -H "Content-Type: application/zip" \
+    -d @$assetName \
+    https://uploads.github.com/repos/hmcts/${repoName}/releases/${releaseId}/assets?name=${assetName}
+}
+
 zip -r -j civil-damages-camunda-bpmn-definition.zip src/main/resources/camunda
 
-currentVersion=$(curl --silent "https://api.github.com/repos/hmcts/civil-damages-camunda-bpmn-definition/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-IFS='.' read -a versionParts <<< "$currentVersion"
-patchVersion=$((versionParts[2] + 1))
-nextVersion="${versionParts[0]}.${versionParts[1]}.${patchVersion}"
+az login --identity
+releaseId=$(createNewRelease civil-damages-camunda-bpmn-definition)
 
-gh release create ${nextVersion} \
-  --title civil-damages-camunda-bpmn-definition-v${nextVersion} \
-  --notes TO-BE-UPDATED-WITH-BUILD-LINK \
-  civil-damages-camunda-bpmn-definition.zip
+uploadReleaseAsset civil-damages-camunda-bpmn-definition $releaseId civil-damages-camunda-bpmn-definition.zip
 
 rm civil-damages-camunda-bpmn-definition.zip
